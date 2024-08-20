d449b2a9a0dfe511744af188bcc7aa1bb422b4df
felipemadeit <felipemadeit@gmail.com>
2024-08-19 19:04:36 -0500

    El crud fue creado exitosamente y testeado con postman

--- src/main/java/com/crudspring/apicrud/Controllers/ProductController.java
+++ src/main/java/com/crudspring/apicrud/Controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.crudspring.apicrud.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -74,7 +75,7 @@ public class ProductController {
 
         productRepository.delete(product);
 
-        return "The product with the id " + id + 2 "was deleted";
+        return "The product with the id  " + id +  "was deleted";
 
     }
     
